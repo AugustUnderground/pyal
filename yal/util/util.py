aafb133418d11f9111bcd7b5a095bd002bbb8a29
Yannick Uhlmann <augustunderground@protonmail.com>
2024-01-18 11:22:31 +0100

    fix typos in doc

--- yal/util/util.py
+++ yal/util/util.py
@@ -32,13 +32,11 @@ def randomize_placement( participant:dict, x_bound: int = 120, y_bound: int = 12
     """
     Randomize the placement of a participant within a given boundary.
 
-    Arguemnts:
+    Arguments:
 
-        `participant`: The participant whose placement will be randomized
-
-        `x_bound`: X-Axis upper limit. (optional, default = 120)
-
-        `y_bound`: Y-Axis upper limit. (optional, default = 120)
+    - `participant`: The participant whose placement will be randomized
+    - `x_bound`: X-Axis upper limit. (optional, default = `120`)
+    - `y_bound`: Y-Axis upper limit. (optional, default = `120`)
     """
     x_limit = x_bound - participant['width']
     y_limit = y_bound - participant['height']
@@ -49,18 +47,17 @@ def randomize_placement( participant:dict, x_bound: int = 120, y_bound: int = 12
 def as_participant( module: core.Module, opt_fields: list[str] = None
                   , colorize: bool = True ) -> dict:
     """
-    Convert a `Module` dataclass object to a `dict` participant.
+    Convert a `yal.core.Module` dataclass object to a `dict` participant.
 
     Arguments:
 
-        `module`: The `Module` object to convert
-
-        `opt_fields`: Optional field names (as string) from `Module` to retain
-            in the participant dict. Options are 'module_type', 'dimensions',
-            'terminals', 'network', 'placement' and 'critical_nets'. Default is
-            `None`.
-
-        `colorize`: Whether to add a random color to the participant (optional, default = True)
+    - `module`: The `yal.core.Module` object to convert
+    - `opt_fields`: Optional field names (as string) from `yal.core.Module`
+        to retain in the participant dict. Options are `'module_type'`,
+        `'dimensions'`, `'terminals'`, `'network'`, `'placement'` and
+        `'critical_nets'`. (optional, default is `None`)
+    - `colorize`: Whether to add a random color to the participant. Otherwise
+        the `'color'` field is `None`. (optional, default = True)
 
     Return:
 
@@ -110,28 +107,26 @@ def as_participants( modules: list[core.Module]
                    , critical_nets: bool = False
                    ) -> list[dict]:
     """
-    Read a .yal file and convert it to a list of dictionaries.
+    Convert a list of `yal.core.Module`s to a list of dictionaries.
 
-    Arguemnts:
+    Arguments:
 
-        `modules`: A list of `Module` objects.
-
-        `rng_placement`: A boolean indicating whether the initial placement
-            shall be randomized. (optional, default = True)
-
-        `colorize`: Whether to add a random color to the participant (optional, default = True)
-
-        `module_type`: Whether to retain the 'module_type' field (optional, default = False)
-
-        `dimensions`: Whether to retain the 'dimensions' field (optional, default = False)
-
-        `terminals`: Whether to retain the 'terminals' field (optional, default = False)
-
-        `network`: Whether to retain the 'network' field (optional, default = True)
-
-        `placement`: Whether to retain the 'placement' field (optional, default = False)
-
-        `critical_nets`: Whether to retain the 'critical_nets' field (optional, default = False)
+    - `modules`: A list of `yal.core.Module` objects.
+    - `rng_placement`: A boolean indicating whether the initial placement
+        shall be randomized. (optional, default = `True`)
+    - `colorize`: Whether to add a random color to the participant (optional, default = `True`)
+    - `module_type`: Whether to retain the `yal.core.Module.module_type` field
+        (optional, default = `False`)
+    - `dimensions`: Whether to retain the `yal.core.Module.dimensions` field
+        (optional, default = `False`)
+    - `terminals`: Whether to retain the `yal.core.Module.terminals` field
+        (optional, default = `False`)
+    - `network`: Whether to retain the `yal.core.Module.network` field
+        (optional, default = `True`)
+    - `placement`: Whether to retain the `yal.core.Module.placement`
+        field (optional, default = `False`)
+    - `critical_nets`: Whether to retain the `yal.core.Module.critical_nets` field
+        (optional, default = `False`)
     """
 
     all_fields = zip( [ module_type, dimensions, terminals
